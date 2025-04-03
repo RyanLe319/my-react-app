5ecd4ad59aa67ca1334c869b534ef3015075dc96
RyanLe319 <Ryanle1903@gmail.com>
2025-04-02 20:12:26 -0700

    This should be the latest version that completely works, start branching off from here

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/my-react-app/", // Added trailing slash (critical for GitHub Pages)
 	plugins: [react()],
-	build: {
-		outDir: "dist", // Explicitly set output directory
-		emptyOutDir: true, // Clears dist folder before build
-	},
+
 });
